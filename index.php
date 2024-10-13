80316894bcce9abe048bb11ddcb9aa39d4918d3f
Fatiya Labibah <fatiyalabibah17@gmail.com>
2024-10-13 14:06:57 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+$name = "fatiya labibah";
+
+$animals = ["kambing", "kuda", "bebek"];
+foreach ($animals as $animal)
+
+echo $animal.PHP_EOL;
+echo "<br>";
+echo $name;
+
+function hitung($a, $b) {
+    return $a + $b;
+}
+
+$result = hitung(2,3);
+echo $result.PHP_EOL;
